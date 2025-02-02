a12557ce0997d076b89c35d69058775ab3c56c16
Ilja Kravecs <ilya.kravecs@gmail.com>
2025-02-02 22:56:14 +0200

    Resolve comments and update loop.js and objects.ts and fix eslint errors

--- 4thHomeWork/src/loop.js
+++ 4thHomeWork/src/loop.js
@@ -5,3 +5,25 @@ for (let i = 0; i <= 9; i++) {
 for (let i = 100; i >= 0; i -= 10) {
     console.log(i);
 }
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+for (const num of numbers) {
+    console.log(num);
+}
+
+const student = { name: "Ilja", age: 34, course: "QAT TypeScript" };
+for (const key in student) {
+    console.log(key + ": " + student[key]);
+}
+
+let x = 0;
+while (x <= 9) {
+    console.log(x);
+    x++;
+}
+
+let y = 100;
+do {
+    console.log(y);
+    y -= 10;
+} while (y >= 0);
--- 4thHomeWork/src/objects.js
+++ 4thHomeWork/src/objects.js
@@ -1,5 +1,5 @@
-let complexObject = {
-    name: "Ilja Kravecs",
+const complexObject = {
+    name: "Ilja K.",
     age: 34,
     address: {
         country: "Latvia",
@@ -7,16 +7,31 @@ let complexObject = {
         city: "Riga",
         postalCode: "LV-0000"
     },
-    hobbies: ["hockey", "cycling", "coding", "gaming"],
-    displayInfo: function() {
-        console.log("Name: " + this.name);
-        console.log("Age: " + this.age);
-        console.log("Address: " + this.address.country + ", " + this.address.street + ", " + this.address.city + ", " + this.address.postalCode);
-        console.log("Hobbies: ");
-        this.hobbies.forEach(function(hobby) {
-            console.log("- " + hobby);
-        });
-    }
+    hobbies: ["hockey", "cycling", "coding", "gaming"]
 };
 
-complexObject.displayInfo();
+const clone1 = Object.assign({}, complexObject);
+const clone2 = { ...complexObject };
+
+clone1.name = "Elijah K.";
+clone1.address.street = "Lidotaju str.";
+clone1.hobbies = ["hockey", "cycling"];
+clone2.name = "Elijah2";
+clone2.address.city = "Jurmala";
+clone2.hobbies = "diving";
+
+console.log(clone1, complexObject);
+console.log("----------\n");
+console.log(clone2, complexObject);
+
+const clone3 = JSON.parse(JSON.stringify(complexObject));
+clone3.name = "Ilias K.";
+clone3.address.city = "Madrid";
+clone3.hobbies = ["coding", "gaming"];
+
+console.log("----------\n");
+console.log(clone3, complexObject);
+
+console.log(complexObject === clone1);
+console.log(complexObject.street === clone1.street);
+console.log(typeof complexObject, typeof clone3.name);
