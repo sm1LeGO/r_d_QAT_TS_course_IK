574b00b57f770aedad42048ea8fd48fa28c57239
Ilja Kravecs <ilya.kravecs@gmail.com>
2025-03-17 15:38:45 +0200

    Initial commit which contains solution for both 15 and 16 homeworks

---
+++ 15th-16thHomeWork/tests/cypress/cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+    projectId: 'n4iypy',
+    e2e: {
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        }
+    }
+});
